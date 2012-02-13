3684d23a1b97660dc02d2574611dcbf52d20f38e
Zoom.Quiet <zoomquiet+git@gmail.com>
2012-02-13 16:52:05 +0800

    yint fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n URIsaok base KSC");
+  res.end("Hello world\n URIsaok base KSC \n{v12.02.13}");
 });
  
 server.listen(process.env.PORT || 8001);
