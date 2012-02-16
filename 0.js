1f73b4709b6aefae7dc0f4e888d04a052397dd0f
Zoom.Quiet <zoomquiet+git@gmail.com>
2012-02-16 20:04:17 +0800

    try consloe.log

--- 0.js
+++ 0.js
@@ -4,4 +4,5 @@ var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Hello world\n URIsaok base KSC \n{v12.02.13}");
 });
+console.log("hollooooo")
 server.listen(process.env.PORT || 8001);
