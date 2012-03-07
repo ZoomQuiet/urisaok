4514103b8f7ec15c679edeb52143150228acb7d7
Zoom.Quiet <zoomquiet+git@gmail.com>
2012-03-08 00:40:43 +0800

    action all

--- lua/chk.lua
+++ lua/chk.lua
@@ -1,84 +1,9 @@
 -- try openresty easy creat RESTful API srv.
 ngx.req.read_body()
-VERTION="URIsAok4openresty v12.03.6"
-PHISHTYPE = {["-1"]='UNKNOW'
-    ,["0"]='GOOD'
-    ,["1"]='PHISH'
-    ,["2"]='MAYBE PHISH'
-    }
---ngx.say(PHISHTYPE["2"])
-APPKEY = "k-60666"
-SECRET = "99fc9fdbc6761f7d898ad25762407373"
-ASKHOST = "http://open.pc120.com"
-ASKTYPE = "/phish/?"
-
-
 local method   = ngx.var.request_method
 ngx.say("remote_addr:\t",ngx.var.remote_addr)
 --ngx.log(ngx.INFO,"\n\trequest_method: ",method,"\n\t")
-if method == 'POST' then
-    local data = ngx.req.get_body_data()
-    ngx.say("get_body_data:\t",data)
-
-    local args = ngx.req.get_post_args()
-    local uri = args.uri
-    ngx.say("get_post_args:\t",args.uri)
-    ngx.say("md5:\t",ngx.md5(args.uri))
-    ngx.say("encode_base64:\t",ngx.encode_base64(args.uri))
-    ngx.say("ngx.now():\t",ngx.now())
-
-    --urili = _splitstr(uri,"/")
-    local str, sep = args.uri, "/"
-    print (str, sep)
-    fields = {}
-    str:gsub("([^"..sep.."]*)"..sep, function(c) table.insert(fields, c) end)
-    --for k,v in next,  fields do ngx.say(v) end
-    ngx.say("str:gsub\t",fields[3])
-    --ngx.say("string._split:\t",urili[3])
-    --[[
-    ok, html = _fetch_uri("http://open.pc120.com/phish/")
-    if ok then
-        ngx.say("KCS /phish?:\t",html)
-    end
-    ]]
-
-else
-    ngx.say('only POST chk me;-)')
-end
-
--- intr. func.
-function _splitstr(restr,regexp )
-    -- body
-    --assert(restr ~= '')
-    --assert(regexp ~= '')
-    local str, sep = restr, regexp
-    print (str, sep)
-    fields = {}
-    str:gsub("([^"..sep.."]*)"..sep, function(c) table.insert(fields, c) end)
-    return fields
-end
-
-function string:_split(sSeparator, nMax, bRegexp)
-    assert(sSeparator ~= '')
-    assert(nMax == nil or nMax >= 1)
-    local aRecord = {}
-    if self:len() > 0 then
-        local bPlain = not bRegexp
-        nMax = nMax or -1
-        local nField=1 nStart=1
-        local nFirst,nLast = self:find(sSeparator, nStart, bPlain)
-        while nFirst and nMax ~= 0 do
-            aRecord[nField] = self:sub(nStart, nFirst-1)
-            nField = nField+1
-            nStart = nLast+1
-            nFirst,nLast = self:find(sSeparator, nStart, bPlain)
-            nMax = nMax-1
-        end
-        aRecord[nField] = self:sub(nStart)
-    end
-    return aRecord
-end
-
+-- intra. func.
 curl = require "luacurl"
 function _fetch_uri(url, c)
     local result = { }
@@ -93,4 +18,50 @@ function _fetch_uri(url, c)
     end)
     local ok = c:perform()
     return ok, table.concat(result)
+end
+function _split(str, sep)
+    fields = {}
+    str:gsub("([^"..sep.."]*)"..sep, function(c) table.insert(fields, c) end)
+    return fields
+end
+-- global var
+PHISHTYPE = {["-1"]='UNKNOW'
+    ,["0"]='GOOD'
+    ,["1"]='PHISH'
+    ,["2"]='MAYBE PHISH'
+    }
+--ngx.say(PHISHTYPE["2"])
+APPKEY = "k-60666"
+SECRET = "99fc9fdbc6761f7d898ad25762407373"
+ASKHOST = "http://open.pc120.com"
+ASKTYPE = "/phish/?"
+function checkForValidUrl(uri)
+    crtURI = ngx.encode_base64(uri)
+    timestamp = ngx.now()
+    signbase = ASKTYPE .. "appkey=" .. APPKEY .. "&q=" .. crtURI .. "&timestamp=" .. timestamp
+    sign = ngx.md5(signbase .. SECRET)
+    return ASKHOST .. signbase .. "&sign=" .. sign
+end
+
+if method ~= 'POST' then
+    ngx.say('only POST chk me;-)')
+    local readme = ngx.location.capture("/")
+    if res.status == 200 then
+        ngx.say(readme.body)
+    end
+else
+    local data = ngx.req.get_body_data()
+    ngx.say("get_body_data:\t",data)
+    local args = ngx.req.get_post_args()
+    local uri = args.uri
+    ngx.say("get_post_args:\t",args.uri)
+    fields = _split(uri,"/")
+    ngx.say("str:gsub\t",fields[3])
+    local url = fields[3]
+    local chkURI = checkForValidUrl(url)
+    ngx.say(chkURI)
+    ok, html = _fetch_uri(chkURI)
+    if ok then
+        ngx.say("KCS /phish?:\t",html)
+    end
 end
