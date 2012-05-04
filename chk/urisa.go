e77906c8b2455bc97484994692e3f80d91bb0e03
Zoom.Quiet <zoomquiet+git@gmail.com>
2012-05-04 10:51:17 +0800

    done /chk

--- chk/urisa.go
+++ chk/urisa.go
@@ -1,4 +1,4 @@
-package hollo
+package urisa
 
 import (
     "fmt"
@@ -21,7 +21,6 @@ import (
 func init() {
     http.HandleFunc("/", help)
     http.HandleFunc("/chk", chk)
-    http.HandleFunc("/try", try)
 }
 
 func help(w http.ResponseWriter, r *http.Request) {
@@ -29,6 +28,7 @@ func help(w http.ResponseWriter, r *http.Request) {
 }
 const usageHelp = `
 URIsA ~ KSC 4 GAE powdered by go1
+{v12.05.3}
 usage:
     $ curl -d "uri=http://sina.com" urisago1.appsp0t.com/chk
 `
@@ -37,6 +37,12 @@ var APPKEY  = "k-60666"
 var SECRET  = "99fc9fdbc6761f7d898ad25762407373"
 var APIHOST = "open.pc120.com"
 var APITYPE = "/phish/"
+var PHISHID = map[int] string {
+    -1:   "UNKNOW",
+    0:    "GOOD",
+    1:    "PHISH!",
+    2:    "MAYBE...",
+}
 //var HOSTTRY = "127.0.0.1:1978"//"urisaok.no.de"
 func chk(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
@@ -57,8 +63,8 @@ func chk(w http.ResponseWriter, r *http.Request) {
     //c.Infof("base64~ %v\n", len(strings.TrimSpace(code64)))
     //c.Infof("base64~ %v\n", len(strings.Trim(code64, "\0000")))
     //c.Infof("base64~ %v\n", code64[0:len(strings.Trim(code64, "\0000"))])
-    args := "q=" + string(dst)
-    args += "&appkey=" + APPKEY
+    args := "appkey=" + APPKEY
+    args += "&q=" + string(dst)
     now := time.Now()
     //c.Infof("%v , %v", now.Unix(), now.UnixNano())
     nano := strconv.FormatInt(now.UnixNano(),10)
@@ -66,7 +72,7 @@ func chk(w http.ResponseWriter, r *http.Request) {
     args += "&timestamp=" + nano[0:10] + "." + nano[10:13]
     sign_base_string := APITYPE + "?" + args 
     c.Infof("sign_base_string~\t %v\n", sign_base_string)
-    //md5 hash
+    //md5 hash 严格参数顺序:: appkey -> q -> timestamp
     h := md5.New()
     io.WriteString(h, sign_base_string + SECRET)
     //tmp := "/phish/?appkey=YXNkZmFzZGZqYXM&q=aHR0cDovL3NoZW56aGVuLWd6Yy5pbmZv&timestamp=1295430113.546"
@@ -95,24 +101,9 @@ func chk(w http.ResponseWriter, r *http.Request) {
     buf, _ = ioutil.ReadAll(resp.Body)
     c.Infof("resp.Body %v", string(buf))
 
-    fmt.Fprint(w, "[DEBUG]\t"+string(buf))
- 
-    //fmt.Fprint(w, "uri=\t"+r.FormValue("uri"))
-    }
-/*
-    req, err := http.NewRequest("POST", "http://urisaok.appsp0t.com/chk", nil)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    req.Header.Set("use_intranet", "yes")
-    t := &urlfetch.Transport{Context: appengine.NewContext(r)}
-    resp, err := t.RoundTrip(req)
-
-
     type KSC struct {
         Success int    //`json:"success"`
-        Phish   string //`json:"phish"`
+        Phish   int //`json:"phish"`
         Msg     string //`json:"msg"`
     }
     result := &KSC{}
@@ -122,23 +113,13 @@ func chk(w http.ResponseWriter, r *http.Request) {
         //http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    c.Infof("Success:%v \n Phish:%v", result.Success ,result.Phish)
+    //c.Infof("PHISHID:\t %v %v", result.Phish, PHISHID[0])
+    pishmsg, _ := PHISHID[result.Phish]
+    c.Infof("Success:%v \n Phish:%s", result.Success ,pishmsg)
 
-    h := md5.New()
-    io.WriteString(h, "The fog is getting thicker!")
-    fmt.Printf("%x", h.Sum(nil))
-
-   //base64 encode...
-    //var buf = bytes.NewBufferString(url)
-    var encoder = base64.NewEncoder(base64.NewEncoding(url), &buf)
-    encoder.Encode()
-    //encoder.Close()
-    c.Infof("base64: %v", buf.String())
-
-    var URLEncoded = base64.NewEncoding(url)
-    enc := URLEncoded.EncodeToString()
-    c.Infof("enc: %v %v", len(enc), string(enc)) 
-
+    fmt.Fprint(w, "/chk(KCS):\t" + pishmsg)
+    }
+/*
 def __genQueryArgs(api_path, url):
     args = "appkey=" + cfg.APPKEY
     args += "&q=" + base64.urlsafe_b64encode(url)
@@ -158,72 +139,3 @@ def _askCloud(api_path, url):
     else:
         return result
 */
-
-func try(w http.ResponseWriter, r *http.Request) {
-    //fmt.Fprint(w, "uri=\t"+r.FormValue("uri"))
-    c := appengine.NewContext(r)
-    client := urlfetch.Client(c)
-    resp, err := client.Get("http://open.pc120.com/phish/")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    //fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)    
-    c.Infof("HTTP GET returned status %v", resp.Status)
-    if resp.StatusCode != 200 {
-        http.Error(w, "couldn't get sale data", http.StatusInternalServerError)
-        return
-    }
-
-    defer resp.Body.Close()
-
-    c.Infof("resp.ContentLength %v", resp.ContentLength)
-
-    var buf []byte
-    buf, _ = ioutil.ReadAll(resp.Body)
-    c.Infof("resp.Body %v", string(buf)) 
-
-    //var res KSC
-    type KSC struct {
-        Success int    //`json:"success"`
-        Phish   string //`json:"phish"`
-        Msg     string //`json:"msg"`
-    }
-    //json.Unmarshal(body, result)
-    //json.Unmarshal(body, &res); 
-    result := &KSC{}
-    err = json.Unmarshal(buf, result)
-    if err != nil {
-        //panic(err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    c.Infof("%+v", result.Msg)
-
-    fmt.Fprintf(w, "HTTP GET returned :: %v", resp.Body)
-       
-}
-  
-    // The message body.    Body io.ReadCloser
-    /**
-    var f interface{}
-    json.Unmarshal(body, &f)
-    m := f.(map[string]interface{})
-    for k, v := range m {
-        switch vv := v.(type) {
-        case string:
-            c.Infof("%v is str %v", k, vv)
-        case int:
-            c.Infof("%v is int %v", k, vv)
-        case []interface{}:
-            c.Infof("%v is an array:",k )
-            for i, u := range vv {
-                c.Infof("%v %v ", i, u)
-            }
-        default:
-            c.Infof(k, "is of a type I don't know how to handle")
-        }
-    }
-
-    **/
